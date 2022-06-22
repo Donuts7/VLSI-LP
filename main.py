6c17238c9a10389c957216ae7e441802971f6542
pc <alessandro.calo98@outlook.com>
2022-06-22 18:36:04 +0200

    primo push vero sul mio branch

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from pulp import *
+# così il problema non trova una soluzione, se togli il constraint linea 40 invece si (trova 0 ma non va bene ovviamente)
 
+# questi sono dati hardcodati, poi quando facciamo funzionare metteremo la possibilità di leggere da file
 width = 8
 n_circuits = 4
 circuit_x = [3, 3, 5, 5]
 circuit_y = [3, 5, 3, 5]
-
 max_y = 5
 
 prob = LpProblem("VLSI", LpMinimize)
 
+# questa all'inizio era una LpVariable ma mi dava errore quando la moltiplicavo (linee 38 e 39)
 height = sum(circuit_y)
 x_coord = [LpVariable("x{}".format(i + 1), 1, max(circuit_x), LpInteger) for i in range(n_circuits)]
 y_coord = [LpVariable("y{}".format(i + 1), 1, max_y, LpInteger) for i in range(n_circuits)]
 
+# non so come fare in modo che queste due siano variabili booleane
 x_pair = [[LpVariable("x{}{}".format(i + 1, j + 1), 0, 1, LpBinary) for j in range(n_circuits)]for i in range(n_circuits)]
 y_pair = [[LpVariable("y{}{}".format(i + 1, j + 1), 0, 1, LpBinary) for j in range(n_circuits)]for i in range(n_circuits)]
 
-# test branch ale
 
 
+# questa è da minimizzare
 prob += height, "Height of the plate"
 
 #prob += [x_coord[i]+circuit_x[i] <= width for i in range(n_circuits)],"Respect width"
@@ -34,7 +37,7 @@ for i in range(n_circuits):
             prob += x_coord[i] + circuit_x[i] <= x_coord[j] + width * (x_pair[i][j] + y_pair[i][j])
             prob += x_coord[i] - circuit_x[j] >= x_coord[j] - width * (1 - x_pair[i][j] + y_pair[i][j])
             prob += y_coord[i] + circuit_y[i] <= y_coord[j] + height * (1 + x_pair[i][j] - y_pair[i][j])
-            prob += y_coord[i] - circuit_y[j] >= y_coord[j] - height * (2 - x_pair[i][j] - y_pair[i][j])
+           # prob += y_coord[i] - circuit_y[j] >= y_coord[j] - height * (2 - x_pair[i][j] - y_pair[i][j])
 
 prob.solve()
 
