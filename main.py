177e292ac557d8486534048b6643209f8df195f2
pc <alessandro.calo98@outlook.com>
2022-06-28 23:34:40 +0200

    tutte le istanze risolte e plottate in plotted-out

--- main.py
+++ main.py
@@ -1,67 +1,82 @@
+import pulp as pl
 from pulp import *
 from utils import *
-# così il problema non trova una soluzione, se togli il constraint linea 40 invece si (trova 0 ma non va bene ovviamente)
-ins_number=2
-
-d = get_data(ins_number)
-
-width = d['width']
-n_circuits = d['n_circuits']
-circuit_x = d["circuit_x"]
-circuit_y = d["circuit_y"]
-max_y = d['max_y']
-
-prob = LpProblem("VLSI", LpMinimize)
-
-# mantengo sia Y che H (come nel video), dove Y è h_to_min e H è height
-height = sum(circuit_y)
-h_to_min = LpVariable("Height",max_y,height,LpInteger)
-
-x_coord = [LpVariable("x{}".format(i + 1), 0, width, LpInteger) for i in range(n_circuits)]
-y_coord = [LpVariable("y{}".format(i + 1), 0, height, LpInteger) for i in range(n_circuits)]
-
-# non so come fare in modo che queste due siano variabili booleane
-x_pair = [[LpVariable("x{}{}".format(i + 1, j + 1), 0, 1, cat="Integer") for j in range(n_circuits)]for i in range(n_circuits)]
-y_pair = [[LpVariable("y{}{}".format(i + 1, j + 1), 0, 1, cat="Integer") for j in range(n_circuits)]for i in range(n_circuits)]
 
 
-# questa è da minimizzare
-prob += h_to_min, "Height of the plate"
+for ins_number in range(1, 41):
 
-for i in range(n_circuits):
-    for j in range(n_circuits):
-        prob += x_coord[i] + circuit_x[i] <= width
-        prob += y_coord[i] + circuit_y[i] <= h_to_min
+    # print(listSolvers(onlyAvailable = True))
 
-        if i < j:
+    d = get_data(ins_number)
 
-            prob += x_coord[i] + circuit_x[i] <= x_coord[j] + width * (x_pair[i][j] + y_pair[i][j])
-            prob += x_coord[i] - circuit_x[j] >= x_coord[j] - width * (1 - x_pair[i][j] + y_pair[i][j])
-            prob += y_coord[i] + circuit_y[i] <= y_coord[j] + height * (1 + x_pair[i][j] - y_pair[i][j])
-            prob += y_coord[i] - circuit_y[j] >= y_coord[j] - height * (2 - x_pair[i][j] - y_pair[i][j])
+    width = d['width']
+    n_circuits = d['n_circuits']
+    circuit_x = d["circuit_x"]
+    circuit_y = d["circuit_y"]
+    max_y = d['max_y']
 
-prob.solve()
+    prob = LpProblem("VLSI", LpMinimize)
 
-'''
-for i in range(n_circuits):
-    for j in range(n_circuits):
-        if x_pair[i][j].varValue != None:
-            print(x_pair[i][j], '=', x_pair[i][j].varValue, ' ', y_pair[i][j], '=', y_pair[i][j].varValue)
+    solver_list = pl.listSolvers()
+    path_to_cplex = r"C:\Program Files\IBM\ILOG\CPLEX_Studio221\cplex\bin\x64_win64\cplex.exe"
+    solver = pl.CPLEX_CMD(path=path_to_cplex,timelimit=180)
+    #solver = pl.get_solver('CPLEX_PY')
+    #solver.buildSolverModel(prob)
 
-for i in range(n_circuits):
-      print(x_coord[i], '=', x_coord[i].varValue, ' ', y_coord[i], '=', y_coord[i].varValue)
-'''
-out = ""
-out += "{} {}\n".format(width, int(h_to_min.varValue))
+    # Modify the solvermodel
+   # solver.solverModel.parameters.timelimit.set(60)
 
-for i in range(n_circuits):
-    out += "{} {} {} {}\n".format(circuit_x[i], circuit_y[i], int(x_coord[i].varValue), int(y_coord[i].varValue))
 
-out += "----------"
 
-print(out)
-"""    
-for v in prob.variables():
-    if len(v.name) == 2:
-        print(v.name, "=", v.varValue)
-"""
+
+
+    # mantengo sia Y che H (come nel video), dove Y è h_to_min e H è height
+    height = sum(circuit_y)
+    h_to_min = LpVariable("Height", max_y, height, LpInteger)
+
+    x_coord = [LpVariable("x{}".format(i + 1), 0, width, LpInteger) for i in range(n_circuits)]
+    y_coord = [LpVariable("y{}".format(i + 1), 0, height, LpInteger) for i in range(n_circuits)]
+
+    # non so come fare in modo che queste due siano variabili booleane
+    x_pair = [[LpVariable("x{}-{}".format(i + 1, j + 1), 0, 1, cat="Integer") for j in range(n_circuits)] for i in
+              range(n_circuits)]
+    y_pair = [[LpVariable("y{}-{}".format(i + 1, j + 1), 0, 1, cat="Integer") for j in range(n_circuits)] for i in
+              range(n_circuits)]
+
+    # questa è da minimizzare
+    prob += h_to_min, "Height of the plate"
+
+    for i in range(n_circuits):
+        for j in range(n_circuits):
+            prob += x_coord[i] + circuit_x[i] <= width
+            prob += y_coord[i] + circuit_y[i] <= h_to_min
+
+            if i < j:
+                prob += x_coord[i] + circuit_x[i] <= x_coord[j] + width * (x_pair[i][j] + y_pair[i][j])
+                prob += x_coord[i] - circuit_x[j] >= x_coord[j] - width * (1 - x_pair[i][j] + y_pair[i][j])
+                prob += y_coord[i] + circuit_y[i] <= y_coord[j] + height * (1 + x_pair[i][j] - y_pair[i][j])
+                prob += y_coord[i] - circuit_y[j] >= y_coord[j] - height * (2 - x_pair[i][j] - y_pair[i][j])
+
+
+    prob.solve(solver)
+    # Solve P
+    #solver.callSolver(prob)
+    #status = solver.findSolutionValues(prob)
+    '''
+    for i in range(n_circuits):
+        for j in range(n_circuits):
+            if x_pair[i][j].varValue != None:
+                print(x_pair[i][j], '=', x_pair[i][j].varValue, ' ', y_pair[i][j], '=', y_pair[i][j].varValue)
+    
+    for i in range(n_circuits):
+          print(x_coord[i], '=', x_coord[i].varValue, ' ', y_coord[i], '=', y_coord[i].varValue)
+    '''
+    out = ""
+    out += "{} {}\n{}\n".format(width, int(h_to_min.varValue), n_circuits)
+
+    for i in range(n_circuits):
+        out += "{} {} {} {}\n".format(circuit_x[i], circuit_y[i], int(x_coord[i].varValue), int(y_coord[i].varValue))
+
+    out += "----------"
+
+    save_data(out, ins_number)
---
+++ out-plot.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+import csv
+import numpy as np
+
+for ins in range(1, 41):
+    a = []
+    with open('my-out\ins-{}-solved.txt'.format(ins), 'r') as fd:
+        reader = csv.reader(fd)
+        for row in reader:
+            if row == ['----------']:
+                break
+            for i in row:
+                a.append(i.replace(" ", ","))
+    ints = []
+    for e in a:
+        ints.append([int(i) for i in e.split(',')])
+
+
+    def create(a):
+        width = int(a[0][0])
+        height = int(a[0][1])
+        length = int(a[1][0])
+
+        s = (width, height)
+        cell = np.ones(s)
+
+        for i in range(length):
+            x_s = a[i + 2][0]
+            y_s = a[i + 2][1]
+            x_c = a[i + 2][2]
+            y_c = a[i + 2][3]
+
+            for y_el in range(y_s):
+                for x_el in range(x_s):
+                    cell[x_c + x_el, y_c + y_el] = int(i + 1)
+        cell = np.rot90(cell)
+        cell = np.flipud(cell)
+        return cell
+
+    legnd = []
+    for el in range(2, len(ints)):
+        legnd.append("size: " + str(ints[el][0]) + ", " + str(ints[el][1]) +
+                     "\ncoordinate: " + str(ints[el][2]) + ", " + str(ints[el][3]))
+
+    try:
+        n_colors = int(ints[1][0])
+        data = create(ints)
+        print(ins)
+
+        cmap = plt.cm.get_cmap('tab20c', n_colors)
+
+        if n_colors >= 19:
+            plt.figure(figsize=(14, 11), dpi=100)
+        else:
+            plt.figure(figsize=(12, 8), dpi=100)
+
+        plt.pcolormesh(data, edgecolors='white', linewidth=0.6, cmap=cmap)
+        plt.title('Instance number {} ({} circuits), {}x{}'.format(ins, n_colors, ints[0][0], ints[0][1]))
+        ax = plt.gca()
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.set_aspect('equal')
+        ax.set_xlabel('Width')
+        ax.set_ylabel('Height')
+
+        # ax.plot(data,label = 'aaal')
+        # ax.legend()
+
+        # ax.legend(data,bbox_to_anchor=(1.04, 1), loc="upper left")
+        import matplotlib.patches as mpatches
+
+        bound = np.linspace(0, 1, n_colors + 1)
+        bound_prep = np.round(bound * (n_colors - 1), 2)
+
+        plt.legend([mpatches.Patch(color=cmap(b)) for b in bound[:-1]],
+                   ['{}'.format(legnd[i], bound_prep[i + 1] - 0.01) for i in range(n_colors)], bbox_to_anchor=(1.04, 1),
+                   loc="upper left")
+
+        plt.rcParams.update({'figure.max_open_warning': 0})
+    except IndexError:
+        print("Empty output")
+    else:
+        plt.savefig('plotted-out\ins-{}-plotted'.format(ins))
+        # plt.show()
+
+
